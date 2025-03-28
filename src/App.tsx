c33cf22f3b5abb550252c8fbc214515c3757e9cf
jakkrish <jak.krish@gmail.com>
2025-03-28 22:50:21 +0530

    functionality added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Modal from './components/Modal';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [data,setData] = useState('This is a demo text content')
   return (
     <>
-      <Modal />
+      <Modal data={data}>Open Modal</Modal>
     </>
   );
 }
--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,5 +1,21 @@
-function Modal() {
-  return <>modal</>;
+import { useState } from "react";
+
+import './Modal.css';
+
+function Modal({data,children}) {
+
+  const [isOpen,setIsOpen] = useState(false)
+
+  const handleClick = ()=>{
+    setIsOpen((prev)=>!prev)
+  }
+  return <>
+  {isOpen && <div className="container">
+    <div className="data"> {data} </div>
+    <div className="close-btn"><button onClick={handleClick}>Close</button></div></div>
+   }
+  <button onClick={handleClick}>{children}</button>
+  </>;
 }
 
 export default Modal;
